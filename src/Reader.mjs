36dbd7c567fcd8405ff1a03e3f67e9154d3a2343
Angel Dominguez <angeldomp49@gmail.com>
2022-06-02 17:20:22 -0500

    readTemplate

--- src/Reader.mjs
+++ src/Reader.mjs
@@ -1,27 +1,18 @@
 
 export const Reader = {
+
+
     readTemplate: function(templateName) {
 
-        let templatePath = "";
-
-        if(fs.existsSync(LOCAL_TEMPLATE_DIR)){
-            console.info("Reading templates from: " + LOCAL_TEMPLATE_DIR);
-            templatePath = this.getTemplatePath(templateName, LOCAL_TEMPLATE_DIR);
-        }
-        else if(fs.existsSync(DEFAULT_TEMPLATE_DIR)){
-            console.info("Reading templates from: " + LOCAL_TEMPLATE_DIR);
-            templatePath = this.getTemplatePath(templateName, DEFAULT_TEMPLATE_DIR);
-        }
-        else{
+        if(! fs.existsSync(templateName)){
             console.error("Template directory not found for: "+ cwd());
             return "";
         }
 
+        let templatePath = this.getTemplatePath(templateName);
+
         return fs.readFileSync(templatePath, "utf8");
         
-    },
-    getTemplatePath: function(templateName, templateDir = DEFAULT_TEMPLATE_DIR) {
-        return templateDir + "/" + templateName + ".temp";
     },
     changePlaceholders: function(content, placeholders) {
 
