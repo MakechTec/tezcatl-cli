0d76b6a74e50f9bc66c46f12d6a5f9856828ddd8
Angel Dominguez <angeldomp49@gmail.com>
2022-06-23 11:49:48 -0500

    insertInLine

--- src/Writter.mjs
+++ src/Writter.mjs
@@ -13,6 +13,22 @@ export const Writter = {
     },
     createDirectory: function(dirName) {
         fs.mkdirSync(dirName, { recursive: true });
+    },
+    insertInLine: function(fileName, lineNumber, text){
+
+        if(Pattern.testHasDirectory(fileName)){
+            let pointer = fileName.lastIndexOf("/");
+            let directory = fileName.substring(0, pointer);
+            this.createDirectory(directory);
+        }
+
+        let originalText = fs.readFileSync(fileName, 'utf8');
+        let lines = originalText.split("\n");
+        let newLines = lines.splice(lineNumber, 0, text);
+        let newText = newLines.join("\n");
+
+        fs.writeFileSync(fileName, newText);
+
     }
     
 };
